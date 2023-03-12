082cbbea10ef99a9ec652d572025a58256231845
lucasleandro1 <lukkassoliveira2215@gmail.com>
2023-03-12 17:06:23 -0300

    Add the greates question

---
+++ the_greates.php
@@ -0,0 +1,10 @@
+<?php
+$v1 = readline();
+$v1 = explode(" ", $v1);
+$maior = -99999999;
+for ($x = 0; $x <= count($v1);$x++){
+  if ($maior < $v1[$x]){
+    $maior = $v1[$x];
+  }
+}
+echo "$maior eh o maior\n";
